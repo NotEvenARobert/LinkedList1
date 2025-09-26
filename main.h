2dbeb5b83857b07bb2cf8d1bdea232860b964044
NotEvenARobert <132989142+NotEvenARobert@users.noreply.github.com>
2025-09-25 20:50:13 -0400

    Update main.h

--- main.h
+++ main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node {
     int data;
     struct node *next;
@@ -9,3 +15,5 @@ node* insert_front(node *root, int item);
 node* reverse(node* head);
 
 void insertToPlace(node* list, int val, int place);
+
+#endif
